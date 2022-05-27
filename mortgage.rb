d5aec3ddfec2599868088767dd5a89414d02aaf0
Max Tan <yenseow@gmail.com>
2022-05-27 00:26:10 +0000

    Updated mortgage calculator

--- mortgage.rb
+++ mortgage.rb
@@ -3,23 +3,30 @@ def prompt(msg)
 end
 
 def valid_integer(num)
+  (num.to_i.to_s == num) && (num.to_i > 0)
+end
+
+def valid_integer_zero_permitted(num)
   num.to_i.to_s == num
 end
 
 def valid_float(num)
-  num.to_f.to_s == num
+  (num.to_f.to_s == num) && (num.to_f > 0)
 end
 
 def valid_num(num)
   valid_integer(num) || valid_float(num)
 end
 
+# Welcome message
+prompt("This is a mortgage calculator.\n")
+
 # Determine the loan amount and convert input to a float value.
 loan_amount = ''
 loop do
   prompt("What is the loan amount in dollars? ")
   loan_amount = gets.chomp
-  break if valid_num(loan_amount) && loan_amount.to_f > 0
+  break if valid_num(loan_amount)
   prompt("Invalid loan amount. Please input a positive number.\n")
 end
 loan_amount = loan_amount.to_f # This is a really important step!
@@ -30,7 +37,7 @@ apr = ''
 loop do
   prompt("What is the Annual Percentage Rate (APR) e.g. 5 for 5%? ")
   apr = gets.chomp
-  break if valid_num(apr) && apr.to_f > 0
+  break if valid_num(apr)
   prompt("Invalid APR. Please input a positive number.\n")
 end
 monthly_interest_rate = apr.to_f / 1200
@@ -45,15 +52,15 @@ prompt("What is the loan duration? Input the years and months.\n")
 loop do
   prompt("How many years do you have left in the loan? ")
   loan_duration_year = gets.chomp
-  break if valid_integer(loan_duration_year) && loan_duration_year.to_i > 0
-  prompt("Invalid number of years. Please input a positive integer.\n")
+  break if valid_integer_zero_permitted(loan_duration_year)
+  prompt("Invalid number of years. Please input an integer.\n")
 end
 
 loop do
   prompt("How many months do you have left in the loan? ")
   loan_duration_month = gets.chomp
-  break if valid_integer(loan_duration_month) && loan_duration_month.to_i > 0
-  prompt("Invalid number of months. Please input a positive integer.\n")
+  break if valid_integer_zero_permitted(loan_duration_month)
+  prompt("Invalid number of months. Please input an integer.\n")
 end
 
 loan_duration_month = (loan_duration_year.to_i * 12) + loan_duration_month.to_i
@@ -64,4 +71,7 @@ monthly_payment = loan_amount *
                   (1 - (1 + monthly_interest_rate)**(-loan_duration_month)))
 
 # Display the monthly payment.
-prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}.")
+prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}.\n")
+
+# Exit message
+prompt("Thanks for using the calculator!")
